6ff11726d6578910b868e2c87176d02474b75897
Carl Johnson <cmjohnson@pbs.org>
2015-11-11 13:35:37 -0500

    Init commit of port-forwarding project

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+var (
+	scheme string
+	host   string
+	port   int
+)
+
+func init() {
+	flag.IntVar(&port, "port", 8000, "Port to reverse proxy")
+	flag.StringVar(&host, "host", "localhost", "Host to reverse proxy")
+	flag.StringVar(&scheme, "scheme", "http", "Scheme to reverse proxy")
+}
+
+func main() {
+	flag.Parse()
+
+	url := &url.URL{
+		Scheme: scheme,
+		Host:   fmt.Sprintf("%s:%d", host, port),
+	}
+
+	LoggingMiddleware := func(s http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Printf("Proxying %s to %s\n", r.URL.Path, url)
+			s.ServeHTTP(w, r)
+		})
+	}
+
+	proxy := LoggingMiddleware(httputil.NewSingleHostReverseProxy(url))
+
+	fmt.Printf("Starting reverse proxy to %s://%s:%d...\n\n", scheme, host, port)
+
+	if err := http.ListenAndServe(":80", proxy); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n\n", err)
+		os.Exit(1)
+	}
+
+}
