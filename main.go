b1972463c8e6ae3d4b852fb82d776b59ad40baff
Carl Johnson <cmjohnson@pbs.org>
2015-11-11 21:43:52 -0500

    Move more things into init
    That way the junk that I don't need after start up can go out of scope and get garbage collected, I hope.

--- main.go
+++ main.go
@@ -12,48 +12,53 @@ import (
 )
 
 var (
-	scheme        string
-	host          string
-	port          int
-	listeningPort int
-	allowExternal bool
+	Url      *url.URL
+	ListenOn string
 )
 
 func init() {
+	var (
+		scheme        string
+		host          string
+		port          int
+		listeningPort int
+		allowExternal bool
+	)
 	flag.IntVar(&port, "port", 8000, "Port to reverse proxy")
 	flag.StringVar(&host, "host", "localhost", "Host to reverse proxy")
 	flag.StringVar(&scheme, "scheme", "http", "Scheme to reverse proxy")
 	flag.IntVar(&listeningPort, "listening-port", 80, "Port to listen on")
 	flag.BoolVar(&allowExternal, "allow-external-connections", false, "Allow other computers to connect to your HTTP server")
-}
-
-func main() {
 	flag.Parse()
 
-	url := &url.URL{
+	Url = &url.URL{
 		Scheme: scheme,
 		Host:   fmt.Sprintf("%s:%d", host, port),
 	}
 
-	var listenOn string
 	if allowExternal {
-		listenOn = fmt.Sprintf(":%d", listeningPort)
+		ListenOn = fmt.Sprintf(":%d", listeningPort)
 	} else {
-		listenOn = fmt.Sprintf("127.0.0.1:%d", listeningPort)
+		ListenOn = fmt.Sprintf("127.0.0.1:%d", listeningPort)
 	}
+
+}
+
+func main() {
 
 	LoggingMiddleware := func(s http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			log.Printf("Proxying %s to %s\n", r.URL.Path, url)
+			log.Printf("Proxying %s to %s\n", r.URL.Path, Url)
 			s.ServeHTTP(w, r)
 		})
 	}
 
-	proxy := LoggingMiddleware(httputil.NewSingleHostReverseProxy(url))
+	proxy := LoggingMiddleware(httputil.NewSingleHostReverseProxy(Url))
 
-	fmt.Printf("Starting reverse proxy from %s to %s...\n\n", listenOn, url)
 
-	if err := http.ListenAndServe(listenOn, proxy); err != nil {
+	fmt.Printf("Starting reverse proxy from %s to %s...\n\n", ListenOn, Url)
+
+	if err := http.ListenAndServe(ListenOn, proxy); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n\n", err)
 		os.Exit(1)
 	}
