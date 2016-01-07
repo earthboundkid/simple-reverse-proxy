d403bb761324a530ded7df7076b21f7f23565a3b
Carl Johnson <cmjohnson@pbs.org>
2016-01-07 10:55:23 -0500

    Minor formatting cleanup

--- main.go
+++ main.go
@@ -12,54 +12,48 @@ import (
 )
 
 var (
-	Url      *url.URL
+	Url      url.URL
 	ListenOn string
 )
 
 func init() {
+	// Set flags
 	var (
-		scheme        string
-		host          string
-		port          int
-		listeningPort int
-		allowExternal bool
+		scheme        = flag.String("scheme", "http", "Scheme to reverse proxy")
+		host          = flag.String("host", "localhost", "Host to reverse proxy")
+		port          = flag.Int("port", 8000, "Port to reverse proxy")
+		listeningPort = flag.Int("listening-port", 80, "Port to listen on")
+		allowExternal = flag.Bool("allow-external-connections", false, "Allow other computers to connect to your HTTP server")
 	)
-	flag.IntVar(&port, "port", 8000, "Port to reverse proxy")
-	flag.StringVar(&host, "host", "localhost", "Host to reverse proxy")
-	flag.StringVar(&scheme, "scheme", "http", "Scheme to reverse proxy")
-	flag.IntVar(&listeningPort, "listening-port", 80, "Port to listen on")
-	flag.BoolVar(&allowExternal, "allow-external-connections", false, "Allow other computers to connect to your HTTP server")
 	flag.Parse()
 
-	Url = &url.URL{
-		Scheme: scheme,
-		Host:   fmt.Sprintf("%s:%d", host, port),
+	Url = url.URL{
+		Scheme: *scheme,
+		Host:   fmt.Sprintf("%s:%d", *host, *port),
 	}
 
-	if allowExternal {
-		ListenOn = fmt.Sprintf(":%d", listeningPort)
+	if *allowExternal {
+		ListenOn = fmt.Sprintf(":%d", *listeningPort)
 	} else {
-		ListenOn = fmt.Sprintf("127.0.0.1:%d", listeningPort)
+		ListenOn = fmt.Sprintf("127.0.0.1:%d", *listeningPort)
 	}
 
 }
 
+func LoggingMiddleware(s http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Reverse proxying %s ...\n", r.URL.Path)
+		s.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	fmt.Printf("Started simple reverse proxy from %s to %s...\n\n", ListenOn, &Url)
 
-	LoggingMiddleware := func(s http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			log.Printf("Reverse proxying %s ...\n", r.URL.Path)
-			s.ServeHTTP(w, r)
-		})
-	}
-
-	proxy := LoggingMiddleware(httputil.NewSingleHostReverseProxy(Url))
-
-	fmt.Printf("Started simple reverse proxy from %s to %s...\n\n", ListenOn, Url)
+	proxy := LoggingMiddleware(httputil.NewSingleHostReverseProxy(&Url))
 
 	if err := http.ListenAndServe(ListenOn, proxy); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n\n", err)
 		os.Exit(1)
 	}
-
 }
