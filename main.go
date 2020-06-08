20c00332567d779d3bda36a501b963711ec0fff3
Carl Johnson <me@carlmjohnson.net>
2020-06-07 20:44:47 -0400

    App: Restructure CLI to use appEnv struct

--- main.go
+++ main.go
@@ -12,44 +12,59 @@ import (
 	"net/url"
 )
 
-var (
-	Url      *url.URL
-	ListenOn string
-	FileName string
-)
-
-func init() {
-	proxyUrl := flag.String("proxy", "http://localhost:8000/", "URL to proxy requests to")
-	listeningPort := flag.Int("listening-port", 80, "Port to listen on")
-	allowExternal := flag.Bool("allow-external-connections", false, "Allow other computers to connect to your HTTP server")
-	flag.Parse()
-
-	var err error
-	Url, err = url.Parse(*proxyUrl)
+func main() {
+	var app appEnv
+	err := app.parseArgs(os.Args[1:])
 	if err != nil {
-		fmt.Fprint(os.Stderr, err)
-		flag.Usage()
+		fmt.Fprintf(os.Stderr, "initialization error: %v\n", err)
 		os.Exit(2)
 	}
+	if err = app.exec(); err != nil {
+		fmt.Fprintf(os.Stderr, "runtime error: %v\n", err)
+		os.Exit(1)
+	}
+}
 
-	if Url.Scheme == "unix" {
-		FileName = Url.Host
+type appEnv struct {
+	URL      *url.URL
+	ListenOn string
+	FileName string
+}
+
+func (app *appEnv) parseArgs(args []string) error {
+	fs := flag.NewFlagSet("simple-reverse-proxy", flag.ContinueOnError)
+	proxyUrl := fs.String("proxy", "http://localhost:8000/", "URL to proxy requests to")
+	listeningPort := fs.Int("listening-port", 80, "Port to listen on")
+	allowExternal := fs.Bool("allow-external-connections", false, "Allow other computers to connect to your HTTP server")
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	var err error
+	app.URL, err = url.Parse(*proxyUrl)
+	if err != nil {
+		fs.Usage()
+		return err
+	}
+
+	if app.URL.Scheme == "unix" {
+		app.FileName = app.URL.Host
 		// Handle URLs written like unix:whatever instead of unix://whatever
-		if FileName == "" {
-			FileName = Url.Opaque
+		if app.FileName == "" {
+			app.FileName = app.URL.Opaque
 		}
-		Url = &url.URL{
+		app.URL = &url.URL{
 			Scheme: "http",
 			Host:   "127.0.0.1",
 		}
 	}
 
 	if *allowExternal {
-		ListenOn = fmt.Sprintf(":%d", *listeningPort)
+		app.ListenOn = fmt.Sprintf(":%d", *listeningPort)
 	} else {
-		ListenOn = fmt.Sprintf("127.0.0.1:%d", *listeningPort)
+		app.ListenOn = fmt.Sprintf("127.0.0.1:%d", *listeningPort)
 	}
-
+	return nil
 }
 
 func LoggingMiddleware(s http.Handler) http.Handler {
@@ -59,26 +74,27 @@ func LoggingMiddleware(s http.Handler) http.Handler {
 	})
 }
 
-func SocketDialer(network, addr string) (conn net.Conn, err error) {
-	return net.Dial("unix", FileName)
+func (app *appEnv) SocketDialer(network, addr string) (conn net.Conn, err error) {
+	return net.Dial("unix", app.FileName)
 }
 
-func main() {
-	proxied := Url.String()
-	rp := httputil.NewSingleHostReverseProxy(Url)
+func (app *appEnv) exec() error {
+	proxied := app.URL.String()
+	rp := httputil.NewSingleHostReverseProxy(app.URL)
 
 	// Handle unix socket connections
-	if FileName != "" {
-		proxied = FileName
+	if app.FileName != "" {
+		proxied = app.FileName
 		rp.Transport = &http.Transport{
-			Dial: SocketDialer,
+			Dial: app.SocketDialer,
 		}
 	}
 
-	fmt.Printf("Started simple reverse proxy from %s to %s...\n\n", ListenOn, proxied)
+	fmt.Printf("Started simple reverse proxy from %s to %s...\n\n",
+		app.ListenOn, proxied)
 
-	if err := http.ListenAndServe(ListenOn, LoggingMiddleware(rp)); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n\n", err)
-		os.Exit(1)
+	if err := http.ListenAndServe(app.ListenOn, LoggingMiddleware(rp)); err != nil {
+		return err
 	}
+	return nil
 }
